48ff25d8000c616113f002fd87805d3249192a25
PondokiOS (Muhammmad Muizzsuddin) <muiz.idn@gmail.com>
2019-07-20 09:07:37 +0700

    https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html

--- kilo.c
+++ kilo.c
@@ -3,36 +3,59 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <errno.h>
 
 struct termios orig_termios;
 
 void die(const char *s) {
-
+  perror(s);
+  exit(1);
 }
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  raw.c_iflag &= ~(
+    IXON // Disable ^S ^Q
+    | ICRNL // Disable ^M,
+    | BRKINT| INPCK | ISTRIP | CS8
+  );
+  raw.c_oflag = ~(
+    OPOST // Disable output processing e.g \n => \r\n
+  );
+  raw.c_lflag &= ~(
+    ECHO // Disable Echoing
+    | ICANON // Disable canonical mode
+    | ISIG // Disable signal ^C ^Z
+    | IEXTEN // Disable ^V (and ^O in macOS)
+    );
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw))
+    die("tcsetattr");
 } 
 
 int main() {
   enableRawMode();
 
-  char c;
-  while ( read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  char c = '\0';
+  while (1) {
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 
+      die("read");
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   }
   return 0;
 }
